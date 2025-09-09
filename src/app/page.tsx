bd25f020468458aa572a90a3d5bd05c3fdf1278b
Vikky Raja <rajvicky97988@gmail.com>
2025-09-09 07:44:33 +0530

    final try update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+
+//forcing new commit
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Indivio - Connecting Dots...</h1>
+      <h1 className="text-4xl font-bold">Indivio - VR Connecting Dots...</h1>
+      <span className="text-2xl">Your gateway to immersive virtual reality experiences.</span>
+      <p className="mt-4 text-lg">Welcome to the Indivio application!</p>
     </main>
   )
 }
